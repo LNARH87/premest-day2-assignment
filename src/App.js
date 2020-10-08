89149d99eb5e5d13214c26038779042301bbdc20
Leslie Narh <leslienarh@hotmail.com>
2020-10-08 04:38:35 +0000

    weather app login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'weather-icons/css/weather-icons.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [isLogin, setIsLogin]=useState();
+
+  function weatherDisplayIn(){
+    setIsLogin(isLogin == true)
+  }
+
+  if (isLogin === false){
+    return (
+      <div className="weather">
+	      <h1>Weather App</h1>
+        <h2>London</h2>
+        <i className="wi wi-day-sunny">25&deg;</i>
+      <form>
+        <input type="text" name="location" placeholder="Enter location here..." />
+        <button type="submit" className="btn btn-primary btn-block btn-large">Submit</button>
+        <br></br>
+        <br></br>
+        <button className="btn btn-primary btn-block btn-large">Sign Out</button>
+    </form>
     </div>
+    )
+  }
+
+  return (
+    <>
+    <div className="login">
+	    <h1>Login</h1>
+      <form>
+    	  <input type="text" name="username" placeholder="Username" />
+        <input type="password" name="password" placeholder="Password"/>
+        <button onClick={weatherDisplayIn} type="submit" className="btn btn-primary btn-block btn-large">Let me in.</button>
+    </form>
+</div>
+    </>
   );
 }
 
